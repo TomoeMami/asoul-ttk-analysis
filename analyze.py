ee9c165f1f1a14e24f2f1b9fd6d67bd2faecefd8
TomoeMami <trembleafterme@outlook.com>
2021-06-23 00:49:36 +0800

    调整了格式

--- analyze.py
+++ analyze.py
@@ -37,13 +37,13 @@ def main():
 
     with open((file_dir+'【'+timedate+'节奏分析】.md').encode('utf-8'),'w',encoding='utf-8') as f:
         f.write('# '+timedate+'节奏分析\n\n')
-        f.write('> ## **本文件最后更新于'+str(lastsave)+'** \n\n')
+        f.write('> # **本文件最后更新于'+str(lastsave)+'** \n\n')
         f.write('本次节奏，共有 **'+str(len(totalreplyers))+'** 人参与，发表了 **'+str(totalreplys)+'** 个回复。\n\n\n')
-        f.write('## 其中，最活跃的前20人回复次数如下表所示：\n\n')
+        f.write('# 其中，最活跃的前20人回复次数如下表所示：\n\n')
         loc20.name = '回复次数'
         loc20.index.name = '昵称'
         f.write(loc20.to_markdown()+'\n\n')
-        f.write('### 按照点赞数排序，这20人的回复中，被点赞前十条分别是： \n\n')
+        f.write('## 按照点赞数排序，这20人的回复中，被点赞前十条分别是： \n\n')
         top20 = all_data_frame.loc[all_data_frame['reply_name'].isin(loc20.index)]
         top20likes = top20.sort_values(by='reply_like',ascending=False)
         top20likes10 = top20likes[:10]
@@ -51,26 +51,27 @@ def main():
             f.write('  **'+top20likes10['reply_name'].iloc[k]+'**  发表于  '+str(top20likes10['reply_time'].iloc[k])+' **'+str(top20likes10['reply_like'].iloc[k])+'** 赞：' +'\n\n')
             f.write('<blockquote> '+top20likes10['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
             f.write('-----\n\n')
-        f.write('## 接下来，让我们看看前十名回复者的具体动态：\n\n')
+        f.write('-----\n\n')
+        f.write('# 接下来，让我们看看前十名回复者的具体动态：\n\n')
         for i in range(10):
             person=all_data_frame.loc[all_data_frame['reply_name']==loc10[i]]
             plikes = person.sort_values(by='reply_like',ascending=False)
             plikes5 = plikes[:5]
-            f.write('### 第'+str(i+1)+'名： **'+loc10[i]+'** \n\n')
+            f.write('## 第'+str(i+1)+'名： **'+loc10[i]+'** \n\n')
             f.write('TA一共回复了 **'+str(loc10d[i])+'** 条消息，在 **'+str(len(totalreplyers))+'** 人中勇夺第 **'+str(i+1)+'** ！ \n\n')
-            f.write('#### 按照点赞数排序，TA回复点赞前五条分别是： \n\n')
+            f.write('### 按照点赞数排序，TA回复点赞前五条分别是： \n\n')
             for k in range(5):
                 f.write(' 发表于'+str(plikes5['reply_time'].iloc[k])+' **'+str(plikes5['reply_like'].iloc[k])+'** 赞：' +'\n\n')
                 f.write('<blockquote> '+plikes5['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
                 f.write('-----\n\n')
-        f.write('## 最后，让我们来看一下点赞前十的评论：\n\n')   
+        f.write('# 最后，让我们来看一下点赞前十的评论：\n\n')   
         ltop10 = all_data_frame.sort_values(by='reply_like',ascending=False)[:10]
         for k in range(10):
             f.write('  **'+ltop10['reply_name'].iloc[k]+'**  发表于  '+str(ltop10['reply_time'].iloc[k])+'  **'+str(ltop10['reply_like'].iloc[k])+'** 赞：' +'\n')
             f.write('<blockquote> '+ltop10['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
             f.write('-----\n\n')
-        f.write('## 特别颁发的奖项\n\n')  
-        f.write('### 深入讨论奖：\n\n') 
+        f.write('# 特别颁发的奖项\n\n')  
+        f.write('## 深入讨论奖：\n\n') 
         f.write('在楼中楼里被他人回复最多次。\n\n') 
         rreply = all_data_frame[all_data_frame['reply_content'].str.contains('回复 @.+ ?')]
         rrstars = rreply['reply_content'].str.extract(r'(@.+:)')
@@ -78,7 +79,7 @@ def main():
         rrstar.name = '回复次数'
         rrstar.index.name = '昵称'
         f.write(rrstar.to_markdown()+'\n\n')    
-        f.write('### 你说EMOJI呢奖：\n\n') 
+        f.write('## 你说EMOJI呢奖：\n\n') 
         f.write('被使用最多次的表情。\n\n') 
         emotes = all_data_frame[all_data_frame['reply_content'].str.contains('\[.+?\]')]
         emotes = emotes['reply_content'].str.extract(r'(\[.+?\])')
@@ -86,14 +87,14 @@ def main():
         emote.name = '使用次数'
         emote.index.name = '表情名称'
         f.write(emote.to_markdown()+'\n\n')   
-        f.write('### 谈笑风生奖：\n\n') 
+        f.write('## 谈笑风生奖：\n\n') 
         f.write('发送带表情的评论最多条数。\n\n') 
         pemotes = all_data_frame[all_data_frame['reply_content'].str.contains('\[.+?\]')]
         pemote = pemotes['reply_name'].value_counts()[:10]
         pemote.name = '带表情评论条数'
         pemote.index.name = '昵称'
         f.write(pemote.to_markdown()+'\n\n')   
-        f.write('## 本次数据统计的采样来源：'+'\n\n')
+        f.write('# 本次数据统计的采样来源：'+'\n\n')
         for thread in threads:
             if(thread['mode'] == 'repost' or thread['mode'] == 'post'):
                 f.write(' https://t.bilibili.com/'+str(thread['oid'])+'\n\n')
