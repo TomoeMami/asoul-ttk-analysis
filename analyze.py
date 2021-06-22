b2d235164e2307f67d4faac47087702e8ba07c1d
TomoeMami <trembleafterme@outlook.com>
2021-06-23 00:21:59 +0800

    2021-06-22

---
+++ analyze.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import os
+import time
+import io
+import os
+
+#以下区域每次使用时更新，复制粘贴
+
+timedate='2021-06-22'
+threads = [
+{'oid':11834829,'mode':'cv'},
+{'oid':539085994140151257,'mode':'repost'},
+{'oid':539081359875657340,'mode':'post'},
+{'oid':539057329536673674,'mode':'post'},
+{'oid':539010780672508914,'mode':'post'}
+]
+
+#复制粘贴到此为止
+
+def main():
+    file_dir = './'+timedate+'/'
+    all_json_list = os.listdir(file_dir)  # get json list
+    for single_json in all_json_list:
+        if(single_json.endswith('.json')):
+            single_data_frame = pd.read_json(os.path.join(file_dir, single_json))
+            if single_json == all_json_list[0]:
+                all_data_frame = single_data_frame
+            else:  # concatenate all json to a single dataframe, ingore index
+                all_data_frame = pd.concat([all_data_frame, single_data_frame], ignore_index=True)
+    totalreplyers = all_data_frame.drop_duplicates(subset=['reply_name'])
+    totalreplys = len(all_data_frame)
+    loc=all_data_frame['reply_name'].value_counts()
+    loc20=loc[:20]
+    loc10=loc[:10].keys()
+    loc10d=loc[:10]
+    lastsave=time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time()+28800))
+
+    with open((file_dir+timedate+'节奏分析.md').encode('utf-8'),'w',encoding='utf-8') as f:
+        f.write('# '+timedate+'\n\n')
+        f.write('> ## **本文件最后更新于'+str(lastsave)+'** \n\n')
+        f.write('本次节奏，共有 **'+str(len(totalreplyers))+'** 人参与，发表了 **'+str(totalreplys)+'** 个回复。\n\n')
+        f.write('## 其中，最活跃的前20人回复次数如下表所示：\n\n')
+        loc20.name = '回复次数'
+        loc20.index.name = '昵称'
+        f.write(loc20.to_markdown()+'\n\n')
+        f.write('### 按照点赞数排序，这20人的回复中，被点赞前十条分别是： \n\n')
+        top20 = all_data_frame.loc[all_data_frame['reply_name'].isin(loc20.index)]
+        top20likes = top20.sort_values(by='reply_like',ascending=False)
+        top20likes10 = top20likes[:10]
+        for k in range(10):
+            f.write('<blockquote><bold>'+top20likes10['reply_name'].iloc[k]+'</bold> 发表于'+str(top20likes10['reply_time'].iloc[k])+'<bold>'+str(top20likes10['reply_like'].iloc[k])+'</bold>赞：' +'\n')
+            f.write(top20likes10['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
+        f.write('## 接下来，让我们看看前十名回复者的具体动态：\n\n')
+        for i in range(10):
+            person=all_data_frame.loc[all_data_frame['reply_name']==loc10[i]]
+            plikes = person.sort_values(by='reply_like',ascending=False)
+            plikes5 = plikes[:5]
+            f.write('### 第'+str(i+1)+'名： **'+loc10[i]+'** \n\n')
+            f.write('TA一共回复了 **'+str(loc10d[i])+'** 条消息，在 **'+str(len(totalreplyers))+'** 人中勇夺第 **'+str(i+1)+'** ！ \n\n')
+            f.write('#### 按照点赞数排序，TA回复点赞前五条分别是： \n\n')
+            for k in range(5):
+                f.write('<blockquote>发表于'+str(plikes5['reply_time'].iloc[k])+'，<bold>'+str(plikes5['reply_like'].iloc[k])+'</bold>赞：' +'\n')
+                f.write(' '+plikes5['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
+        f.write('## 最后，让我们来看一下点赞前十的评论：\n\n')   
+        ltop10 = all_data_frame.sort_values(by='reply_like',ascending=False)[:10]
+        for k in range(10):
+            f.write('<blockquote><bold>'+ltop10['reply_name'].iloc[k]+'</bold>发表于'+str(ltop10['reply_time'].iloc[k])+'，<bold>'+str(ltop10['reply_like'].iloc[k])+'</bold>赞：' +'\n')
+            f.write(' '+ltop10['reply_content'].iloc[k]+ '</blockquote>\n\n\n')
+        f.write('## 特别颁发的奖项\n\n')  
+        f.write('### 深入讨论奖：\n\n') 
+        f.write('在楼中楼里被他人回复最多次。\n\n') 
+        rreply = all_data_frame[all_data_frame['reply_content'].str.contains('回复 @.+ ?')]
+        rrstars = rreply['reply_content'].str.extract(r'(@.+:)')
+        rrstar = rrstars.value_counts()[:10]
+        rrstar.name = '回复次数'
+        rrstar.index.name = '昵称'
+        f.write(rrstar.to_markdown()+'\n\n')    
+        f.write('### 你说EMOJI呢奖：\n\n') 
+        f.write('被使用最多次的表情。\n\n') 
+        emotes = all_data_frame[all_data_frame['reply_content'].str.contains('\[.+?\]')]
+        emotes = emotes['reply_content'].str.extract(r'(\[.+?\])')
+        emote = emotes.value_counts()[:10]
+        emote.name = '使用次数'
+        emote.index.name = '表情名称'
+        f.write(emote.to_markdown()+'\n\n')   
+        f.write('### 谈笑风生奖：\n\n') 
+        f.write('发送带表情的评论最多条数。\n\n') 
+        pemotes = all_data_frame[all_data_frame['reply_content'].str.contains('\[.+?\]')]
+        pemote = pemotes['reply_name'].value_counts()[:10]
+        pemote.name = '带表情评论条数'
+        pemote.index.name = '昵称'
+        f.write(pemote.to_markdown()+'\n\n')   
+        f.write('## 本次数据统计的采样来源：'+'\n\n<blockquote>')
+        for thread in threads:
+            if(thread['mode'] == 'repost' or thread['mode'] == 'post'):
+                f.write('https://t.bilibili.com/'+str(thread['oid'])+'\n')
+            elif(thread['mode'] == 'cv'):
+                f.write('https://www.bilibili.com/read/cv'+str(thread['oid'])+'\n')
+            elif(thread['mode'] == 'av'):
+                f.write('https://www.bilibili.com/video/av'+str(thread['oid'])+'\n')
+        f.write('</blockquote>\n')
+        
+
+
+if __name__ == "__main__":
+    main()
---
+++ getBilibiliComm.py
@@ -0,0 +1,144 @@
+'''
+@Description: 爬取B站弹幕和评论
+@Author: sikaozhifu
+@Date: 2020-06-24 11:29:58
+@LastEditTime: 2020-07-02 19:13:42
+@LastEditors: Please set LastEditors
+'''
+import requests
+import re
+import time
+import math
+import json
+import os
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+}
+
+'''
+@description: 日期格式转换
+@param : value
+@return: dt
+'''
+types={
+    'repost':17,
+    'post':11,
+    'cv':12,
+    'av':1
+}
+
+def mkdir(path):
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+    path=path.encode('utf-8')
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        return False
+#repost代表所有转发，post代表动态。
+def timestamp_datetime(value):
+    format = r'%Y-%m-%d %H:%M:%S'
+    # value为传入的值为时间戳(整形)，如：1332888820
+    value = time.localtime(value)
+    # 经过localtime转换后变成
+    # time.struct_time(tm_year=2012, tm_mon=3, tm_mday=28, tm_hour=6, tm_min=53, tm_sec=40, tm_wday=2, tm_yday=88, tm_isdst=0)
+    # 最后再经过strftime函数转换为正常日期格式。
+    dt = time.strftime(format, value)
+    return dt
+def main():
+
+    #以下区域每次使用时更新，复制粘贴
+
+    timedate='2021-06-22'
+    threads = [{'oid':539081359875657340,'mode':'post'},
+    {'oid':539057329536673674,'mode':'post'},
+    {'oid':11834829,'mode':'cv'},
+    {'oid':539085994140151257,'mode':'repost'},
+    {'oid':539010780672508914,'mode':'post'}
+    ]
+
+    #复制粘贴到此为止
+
+
+    #cv的oid是cv后的数字，无转发动态的oid是地址栏数字。
+    for i in range(len(threads)):
+        oid = threads[i]['oid']
+        gettype = types.get(threads[i]['mode'],None)
+        if gettype == 11:
+            get_url='https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id=%s' % oid
+            response = requests.get(get_url, headers=headers)
+            oid = response.json()['data']['card']['desc']['rid']
+        comment_url = 'https://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn=1&type=%s&oid=%s&sort=1' % (gettype,oid)
+        response = requests.get(comment_url, headers=headers)
+        count = response.json()['data']['page']['count']  # 评论总数
+        page_count = math.ceil(int(count) / 20)  # 评论总页数
+        comment_list = []
+        #追加模式
+        for pn in range(1, page_count + 1):
+            comment_url = 'https://api.bilibili.com/x/v2/reply?pn=%s&type=%s&oid=%s&sort=1' % (pn, gettype,oid)
+            response = requests.get(comment_url, headers=headers)
+            if('data' in response.json().keys()):
+                replies = response.json()['data']['replies']
+                if replies is not None:
+                    for reply in replies:
+                        # reply_id = reply['member']['mid']
+                        # reply_name = reply['member']['uname']
+                        # reply_time = timestamp_datetime(int(reply['ctime']))  # 评论时间
+                        # reply_like = reply['like']  # 评论点赞数
+                        # reply_content = reply['content']['message']  # 评论内容
+                        reply_info = {
+                            'reply_id': reply['member']['mid'],  # 评论者id,
+                            'reply_name': reply['member']['uname'],  # 评论者昵称
+                            'reply_time': timestamp_datetime(int(reply['ctime'])),  # 评论时间
+                            'reply_like': reply['like'],  # 评论点赞数
+                            'reply_content': reply['content']['message']  # 评论内容
+                        }
+                        comment_list.append(reply_info)
+                        rcount = reply['rcount']  # 表示回复的评论数
+                        page_rcount = math.ceil(int(rcount) / 10)  # 回复评论总页数
+                        root = reply['rpid']
+                        for reply_pn in range(1, page_rcount + 1):
+                            time.sleep(1)
+                            reply_url = 'https://api.bilibili.com/x/v2/reply/reply?&pn=%s&type=%s&oid=%s&ps=10&root=%s' % (reply_pn,gettype, oid, root)
+                            response = requests.get(reply_url, headers=headers)
+                            if('data' in response.json().keys()):
+                                rreplies = response.json()['data']['replies']
+                                if rreplies is not None:
+                                    for reply in rreplies:
+                                        # reply_id = reply['member']['mid']  # 评论者id
+                                        # reply_name = reply['member']['uname']  # 评论者昵称
+                                        # reply_time = timestamp_datetime(int(reply['ctime']))  # 评论时间
+                                        # reply_like = reply['like']  # 评论点赞数
+                                        # reply_content = reply['content']['message']  # 评论内容
+                                        reply_info = {
+                                            'reply_id': reply['member']['mid'],  # 评论者id,
+                                            'reply_name': reply['member']['uname'],  # 评论者昵称
+                                            'reply_time': timestamp_datetime(int(reply['ctime'])),  # 评论时间
+                                            'reply_like': reply['like'],  # 评论点赞数
+                                            'reply_content': reply['content']['message']  # 评论内容
+                                        }
+                                        comment_list.append(reply_info)
+                    filedir = './'+timedate+'/'
+                    mkdir(filedir)
+                    if(pn//40 >= 1):
+                        save_path=filedir+str(threads[i]['oid'])+'-'+str(pn//40+1)+'.json'
+                    with open(save_path, "w", encoding='utf-8') as f:
+                        json.dump(comment_list, f, ensure_ascii=False, indent=4, separators=(',', ':'))
+                    # comment_list = [] #改为append模式之后需要及时清空
+            
+
+
+
+if __name__ == "__main__":
+    main()
